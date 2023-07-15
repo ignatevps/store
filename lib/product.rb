cd2142bf076396f0b30156c5f2da479ff9fa156a
Mr Pashu <thepodhiphop@gmail.com>
2023-07-15 11:12:05 +0300

    add hash

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,9 @@
 class Product
   attr_reader :name, :price, :quantity
 
-  def initialize(name, price, quantity)
-    @name = name
-    @price = price
-    @quantity = quantity
+  def initialize(params)
+    @name = params[:name]
+    @price = params[:price]
+    @quantity = params[:quantity]
   end
 end
--- main.rb
+++ main.rb
@@ -2,6 +2,6 @@ require_relative 'lib/product'
 require_relative 'lib/film'
 require_relative 'lib/book'
 
-leon = Product.new("Фильм Леон", 290, 5)
+leon = Product.new(name: "Фильм Леон", price: 290, quantity: 10)
 
-puts "#{leon.name} стоит #{leon.price} рублей"
+puts "#{leon.name} стоит #{leon.price} рублей. Наличие: #{leon.quantity} шт."
